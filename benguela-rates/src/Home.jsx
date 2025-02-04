15f9cccd51718ab2b10876ecfb069242667ce31b
Julian Kataleko <katalekoweb@gmail.com>
2025-02-04 23:07:20 +0100

    feat: currency converter app

---
+++ benguela-rates/src/Home.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+
+const Home = () => {
+    const [currencies, setCurrencies] = useState([])
+    const [from, setFrom] = useState("USD")
+    const [to, setTo] = useState("AOA")
+    const [result, setResult] = useState("")
+
+    useEffect(() => {
+        convert()
+        getCurrencies()
+    }, [])
+
+    const convert = () => {
+        axios.get(`https://api.fastforex.io/fetch-one?api_key=233d45481e-c588f7f6a8-sr60yq&from=${from}&to=${to}`).then((res) => {
+            if (res.data?.result) {
+                setResult(res.data?.result[to])
+            }
+        })
+    }
+
+    const getCurrencies = () => {
+        axios.get(`https://api.fastforex.io/currencies?api_key=233d45481e-c588f7f6a8-sr60yq`).then((res) => {
+            console.log(res);
+            
+            if (res.data?.currencies) {
+                console.log(res.data?.currencies);
+
+                let currencyArray = [];
+
+                for (const obj of Object.entries(res.data?.currencies)) {
+                    // console.log(`${obj}`);
+                    currencyArray.push(obj)
+                  }
+
+                console.log("Array ", currencyArray);                
+                
+                setCurrencies(c => currencyArray)
+
+                console.log("Moedas ",  currencies);
+                
+            }            
+        })
+    }
+
+    const handleFromChange = (event) => {
+        setFrom(event.target.value)
+    }
+
+    const handleToChange = (event) => {
+        setTo(event.target.value)
+    }
+
+  return (
+    <div>
+      <div className="convert-card">
+
+        <div>
+            <input type="text" className='input' placeholder='Enter the amount...' />
+        </div>
+
+        <div className="currencies">
+            <div>
+                From <select style={{width: '100%'}} name="from" id="from" onChange={handleFromChange} value={from}>
+                    {currencies.map((currency, index) => <option value={ currency[0] }> { currency[1] } </option> )}
+                </select>
+            </div>
+            <div>
+                To <select style={{width: '100%'}} name="to" id="to" onChange={handleToChange} value={to}>
+                    {currencies.map((currency, index) => <option value={ currency[0] }> { currency[1] } </option> )}
+                </select>
+            </div>
+        </div>
+
+        <div>
+            <button className='btn' onClick={convert}>Convert</button>
+        </div>
+
+        <div className="result">
+            Result: { result }
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
